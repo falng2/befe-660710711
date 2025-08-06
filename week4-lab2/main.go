87377d469f5026ff3ee9264aaefb9f69d9f3c173
Thanaset Yimprasert <yimprasert_t2@silpakorn.edu>
2025-08-06 05:02:51 +0000

    week4-lab

---
+++ week4-lab2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+// var gomail string = "EMAIL"
+
+func main() {
+	// var name string = "Thanaset"
+	var age int = 20
+
+	email := "yimprasert_t2@su.ac.th"
+	gpa := 2.99
+
+	firstname, lastname := "Thanaset", "Yimprasert"
+
+	fmt.Printf("Name %s %s, age %d, mail %s, gpa %.2f\n", firstname, lastname, age, email, gpa)
+
+}
